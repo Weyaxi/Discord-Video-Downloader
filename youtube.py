e1c11db1a1175062b04799a7c8e8232022e9f6ed
Weyaxi <81961593+Weyaxi@users.noreply.github.com>
2021-05-04 22:09:54 +0300

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from pytube import YouTube
+
+bot = commands.Bot(command_prefix=("!"))
+
+@bot.event
+async def on_ready():
+    print('----------------------------')
+    print(f'{bot.user.name} Olarak Giriş Yapıldı')
+    print(f'Discord Versiyonu {discord.__version__}')
+    print('----------------------------')
+
+@bot.command()
+async def indir(ctx, url):
+    path = ("assets")
+    url = (f"{url}")
+    yt = YouTube(f"{url}")
+
+    try:
+        YouTube(url).streams.first().download(path)
+        await ctx.send(f"**📥 Video Şu Dizine İndiriliyor:** `{path}`")
+        await ctx.send(f"**🎉 Video Adı:** `{yt.title}`")
+        await ctx.send(f"**👍 Video başarıyla belirtilen dizine indirdildi.**")
+    except:
+        await ctx.send(f"**👎 Video indirilemedi. Bir hata ile karşılaşıldı.**")
+
+
+bot.run('ODM5MTE4MTA5MjQ2NzUwNzMw.YJE_ow.gSFiPbo0arEd5qgOruiA0XY2YQw')       
